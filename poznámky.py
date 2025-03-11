7ab7cb92786350f0eb0d1617fa1b2f6ebf3d2964
unknown <vit.basak@infis.cz>
2025-03-11 09:20:02 +0100

    poznámky

---
+++ poznámky.py
@@ -0,0 +1,29 @@
+#načtení od uživatele
+#A = proměnná string
+#B = proměnná intiger
+A = input("Text")
+B = int(input("Číslo"))
+
+#podmínka (if, elif, else):
+if B == "10":
+    print("Číslo je 10")
+elif B <10:
+    print("číslo je menší než 10")
+else:
+    print("číslo je větśí než 10")
+
+# logické operátory (and, or, not)
+if A == "Ahoj" or A == "Čau":
+    print(A)
+else:
+    print("nemám čas")
+
+# vypsání (fce. print)
+print(A)
+print("Jdi spát")
+
+# vkládání na github:
+# git add *
+#git commit -m "text"
+#git push
+# Vít Basák 
